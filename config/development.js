356e60a33a5e81686b73719f7971a31359126294
Federico Scarpa <fedescarpa@gmail.com>
2016-01-26 20:13:06 -0300

    added configuration by environment

---
+++ config/development.js
@@ -0,0 +1,6 @@
+
+angular
+  .module('classroom')
+  .constant('CONFIG', {
+
+  });
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 
+process.env.PORT = process.env.PORT || 8080;
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 const del = require('del');
 const gulp = require('gulp');
 const runSequence = require('run-sequence');
@@ -9,9 +12,17 @@ const $ = gulpLoadPlugins();
 const srcFolder = 'src';
 const outFolder = 'build';
 
+const configFile = () => `config/${process.env.NODE_ENV}.js`;
+
 gulp.task('clean', () => del(`${outFolder}`, { force: true }));
 
-gulp.task('dev:js', () => {
+gulp.task('config', () => {
+  return gulp.src(`${configFile()}`)
+    .pipe($.rename('config.js'))
+    .pipe(gulp.dest(`${srcFolder}/scripts/config`))
+});
+
+gulp.task('dev:js', ['config'], () => {
   return gulp.src([`${srcFolder}/scripts/**/*.js`])
     .pipe($.sourcemaps.init())
       .pipe($.babel({ presets: ['es2015'] }))
@@ -58,13 +69,14 @@ gulp.task('dev:serve', ['dev:build'], () => {
   return gulp.src(`${outFolder}`)
     .pipe($.webserver({
       open: true,
-      port: '9000',
+      port: process.env.PORT,
       host: 'localhost',
       livereload: true
     }));
 });
 
 gulp.task('dev:watch', () => {
+  gulp.watch(`${configFile()}`, ['dev:js']);
   gulp.watch(`${srcFolder}/index.jade`, ['dev:jade:index']);
   gulp.watch(`${srcFolder}/fonts/**/*`, ['dev:fonts']);
   gulp.watch(`${srcFolder}/scripts/**/*.js`, ['dev:js']);
