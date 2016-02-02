0d411320e302cd683d8ed243ce6c72d58e54f996
Agustin Pina <aguspina87@gmail.com>
2016-02-02 15:54:16 -0300

    add production config file

---
+++ config/production.js
@@ -0,0 +1,15 @@
+
+angular
+  .module('classroom')
+  .constant('CONFIG', {
+
+    classroom: {
+      url: 'classroom.mumuki.io'
+    },
+
+    auth: {
+      domain: 'mumuki.auth0.com',
+      clientID: 'w3q8vwGkOmcOi6AEKKgu02iYBzRlea5Q'
+    }
+
+  });
