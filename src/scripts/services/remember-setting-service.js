c80a55ca3f4572726f19fe2b208710de0204f205
Federico Aloi <federico.aloi@gmail.com>
2016-03-07 15:08:51 -0300

    Replaced legacy functions with arrow functions

--- src/scripts/services/remember-setting-service.js
+++ src/scripts/services/remember-setting-service.js
@@ -2,12 +2,16 @@
 angular
   .module('classroom')
   .service('RememberSetting', function($state, $cookies) {
-    return function(scope, key) {
+    return (scope, key) => {
       const cookieName = $state.current.name + "-" + key;
       
       scope[key] = _.get($cookies.getObject(cookieName), 'value'); 
-      scope.$watch(key, function(newValue, oldValue) {
-        newValue ? $cookies.putObject(cookieName, {value: newValue}) : $cookies.remove(cookieName);
+      scope.$watch(key, (newValue, oldValue) => {
+        if (_.isEmpty(newValue)) { 
+          $cookies.remove(cookieName)
+        } else {
+          $cookies.putObject(cookieName, {value: newValue})
+        }
       });
     };
   });
