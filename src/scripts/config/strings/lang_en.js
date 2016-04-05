81253c06c43066c395ff59af324405cad75d8ec6
Federico Scarpa <fedescarpa@gmail.com>
2016-04-05 00:26:43 -0300

    guide progress translated

--- src/scripts/config/strings/lang_en.js
+++ src/scripts/config/strings/lang_en.js
@@ -4,10 +4,14 @@ angular
   .constant('LANG_EN', {
 
     available_courses: 'Available Courses',
+    by_name: 'By name',
+    by_progress: 'By progress',
     copied: 'Copied',
     filter_available_courses: 'Filter available courses',
     filter_available_guides: 'Filter available guides',
+    filter_students: 'Filter students',
     guides: 'Guides',
+    hide_details: 'Hide details',
     log_in: 'Log in',
     log_in_first: 'You must login first',
     log_out: 'Log out',
@@ -19,6 +23,9 @@ angular
     new_course_name_error: 'Course name can only contain letters, numbers, dashes or underscores',
     no_courses_to_show: 'There is no courses to show',
     no_guides_to_show: 'There is no guides to show',
+    no_students_to_show: 'There is no students to show',
+    open_in_atheneum: 'Open in Atheneum',
+    show_details: 'Show details',
     students: 'Students',
     students_subscription_address: 'Students subscription address',
 
