de09409064cfb3dcd82ae696c954d4073ebfd886
Federico Scarpa <fedescarpa@gmail.com>
2016-04-05 00:44:48 -0300

    added spanish lang

---
+++ src/scripts/config/strings/lang_es.js
@@ -0,0 +1,42 @@
+
+angular
+  .module('classroom')
+  .constant('LANG_ES', {
+
+    available_courses: 'Cursos disponibles',
+    by_name: 'Por nombre',
+    by_progress: 'Por progreso',
+    complete_form: 'Cargar datos',
+    copied: 'Copiado',
+    expectations: 'Expectativas',
+    filter_available_courses: 'Filtrar cursos disponibles',
+    filter_available_guides: 'Filtrar guías disponibles',
+    filter_students: 'Filtrar alumnos',
+    first_name: 'Nombre',
+    first_name_error: 'El campo nombre es obligatorio',
+    guides: 'Guías',
+    hide_details: 'Ocultar detalles',
+    last_name: 'Apellido',
+    last_name_error: 'El campo apellido es obligatorio',
+    log_in: 'Iniciar sesión',
+    log_in_first: 'Tenés que iniciar sesión primero',
+    log_out: 'Cerrar sesión',
+    my_courses: 'Mis cursos',
+    new_course: 'Nuevo curso',
+    new_course_description: 'Descripción del curso',
+    new_course_description_error: 'El campo descripción es obligatorio',
+    new_course_name: 'Nombre del curso',
+    new_course_name_error: 'El nombre del curso sólo debe contener letras, números, guiones o guiones bajos',
+    no_courses_to_show: 'No hay cursos para mostrar',
+    no_guides_to_show: 'No hay guías para mostrar',
+    no_students_to_show: 'No hay alumnos para mostrar',
+    open_in_atheneum: 'Abrir en Atheneum',
+    results: 'Resultados',
+    show_details: 'Mostrar detalles',
+    students: 'Alumnos',
+    students_subscription_address: 'Dirección de suscripción de alumnos',
+    submit: 'Enviar',
+    submitted_solutions: 'Soluciones enviadas',
+    tests: 'Pruebas',
+
+  });
--- src/scripts/config/translate.js
+++ src/scripts/config/translate.js
@@ -1,11 +1,13 @@
 
 angular
   .module('classroom')
-  .config(function ($translateProvider, LANG_EN) {
+  .config(function ($translateProvider, LANG_EN, LANG_ES) {
 
+    $translateProvider.translations('es', LANG_ES);
     $translateProvider.translations('en', LANG_EN);
 
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.preferredLanguage('es');
+
     $translateProvider.useSanitizeValueStrategy(null);
 
   });
