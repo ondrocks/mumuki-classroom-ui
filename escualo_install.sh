a554434aede3ed32cd2f000bc9ef97313e58eb94
Franco Leonardo Bulgarelli <flbulgarelli@users.noreply.github.com>
2016-04-05 15:56:06 -0300

    Copying editor config

--- escualo_install.sh
+++ escualo_install.sh
@@ -1,1 +1,11 @@
+#/bin/bash
+REV=$1
+
+echo $REV > version
+
+. /root/.nvm/nvm.sh && npm install --unsafe-perm
 npm run-script build
+
+find -maxdepth 1 | grep -vE 'build$' | grep './' | xargs rm -rf
+mv build/* .
+rm -rf build
