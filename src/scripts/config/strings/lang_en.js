6ebdefa3ab3f5a74b4bba714c800676f0adb8a45
Federico Scarpa <fedescarpa@gmail.com>
2016-04-04 23:39:57 -0300

    home translated

--- src/scripts/config/strings/lang_en.js
+++ src/scripts/config/strings/lang_en.js
@@ -3,7 +3,10 @@ angular
   .module('classroom')
   .constant('LANG_EN', {
 
+    available_courses: 'Available Courses',
+    filter_available_courses: 'Filter available courses',
     log_in: 'Log in',
+    log_in_first: 'You must login first',
     log_out: 'Log out',
     my_courses: 'My courses',
 
