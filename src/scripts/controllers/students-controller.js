19e75a0deb66d4b786e026ede6c621aeee3e7f87
Agustin Pina <aguspina87@gmail.com>
2016-04-06 17:29:51 -0300

    Add students follow system

--- src/scripts/controllers/students-controller.js
+++ src/scripts/controllers/students-controller.js
@@ -1,10 +1,31 @@
 
 angular
   .module('classroom')
-  .controller('StudentsController', function ($scope, $state, $stateParams, students) {
+  .controller('StudentsController', function ($scope, $state, $stateParams, students, Auth, Followers, Api) {
 
     $scope.list = students;
 
-    $scope.sortCriteria = (student) => student.fullName() ;
+    Api.getFollowers(Auth.profile().email)
+      .then((data) => Followers.setFollowers(_.map(data.data.followers, 'social_ids')[0]));
+
+    $scope.sortCriteria = (student) => student.fullName();
+
+    $scope.doFollow = (social_id) => Followers.doFollow(social_id);
+
+    $scope.followClass = (social_id) => $scope.doFollow(social_id) ? 'danger' : 'info';
+
+    $scope.followText = (social_id) => $scope.doFollow(social_id) ? 'unfollow' : 'follow';
+
+    $scope.followAction = (social_id) => $scope.doFollow(social_id) ? $scope.unfollow(social_id) : $scope.follow(social_id)
+
+    $scope.follow = (social_id) =>  {
+    	return Api.follow(social_id, Auth.profile().email)
+    		.then(() => Followers.addFollower(social_id));
+    }
+
+    $scope.unfollow = (social_id) =>  {
+      return Api.unfollow(social_id, Auth.profile().email)
+        .then(() => Followers.removeFollower(social_id));
+    }
 
   });
