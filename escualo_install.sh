1dffed351c5425128b5c35eccfba14b7ffd13898
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-07 13:21:44 -0300

    Saving revision first

--- escualo_install.sh
+++ escualo_install.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 REV=$1
 
+echo '[Escualo::Install::Classroom] Publishing version'
+echo $REV > version
 
 . /root/.nvm/nvm.sh && npm install --unsafe-perm
 npm run-script build
@@ -9,5 +11,3 @@ find -maxdepth 1 | grep -vE 'build$' | grep './' | xargs rm -rf
 mv build/* .
 rm -rf build
 
-echo '[Escualo::Install::Classroom] Publishing version'
-echo $REV > version
