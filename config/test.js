688b55781c16fac17ab6efb9f75934281f665abf
Federico Scarpa <fedescarpa@gmail.com>
2016-04-07 17:06:08 -0300

    added gulp test task

---
+++ config/test.js
@@ -0,0 +1,15 @@
+
+angular
+  .module('classroom')
+  .constant('CONFIG', {
+
+    classroom: {
+      url: 'classroom.localmumuki.io:4000'
+    },
+
+    auth: {
+      domain: 'mumuki.auth0.com',
+      clientID: 'WNmivh61DufjPRhuNZpa10J3LcxbbJmN'
+    }
+
+  });
