5b66c6e514c2eebfdc0373370f979bfe483c6976
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-07 13:09:42 -0300

    Saving revision as last command

--- escualo_install.sh
+++ escualo_install.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 REV=$1
 
-echo $REV > version
 
 . /root/.nvm/nvm.sh && npm install --unsafe-perm
 npm run-script build
@@ -9,3 +8,6 @@ npm run-script build
 find -maxdepth 1 | grep -vE 'build$' | grep './' | xargs rm -rf
 mv build/* .
 rm -rf build
+
+echo '[Escualo::Install::Classroom] Publishing version'
+echo $REV > version
