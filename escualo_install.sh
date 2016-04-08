f9974d55a015288be8305069ba33215bae66c7ce
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-08 10:56:05 -0300

    Not changing paths on build

--- escualo_install.sh
+++ escualo_install.sh
@@ -7,7 +7,3 @@ echo $REV > version
 . /root/.nvm/nvm.sh && npm install --unsafe-perm
 npm run-script build
 
-find -maxdepth 1 | grep -vE 'build$' | grep './' | xargs rm -rf
-mv build/* .
-rm -rf build
-
