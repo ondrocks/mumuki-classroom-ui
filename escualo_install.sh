cf85ba48a4a174433a931972da304ec93fc91b09
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-08 11:34:56 -0300

    Installing npm deps in a shared directory

--- escualo_install.sh
+++ escualo_install.sh
@@ -4,6 +4,6 @@ REV=$1
 echo '[Escualo::Install::Classroom] Publishing version'
 echo $REV > version
 
-. /root/.nvm/nvm.sh && npm install --unsafe-perm
+. /root/.nvm/nvm.sh && npm install --unsafe-perm --prefix /opt/npm-deps
 npm run-script build
 
