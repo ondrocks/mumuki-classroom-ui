d5d3e606a3184be6c36eb4784c4a146e2be718cd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-04-11 14:32:20 -0300

    Extracting courseFollowers

--- src/scripts/services/follower-service.js
+++ src/scripts/services/follower-service.js
@@ -11,10 +11,12 @@ angular
 
     this.isFollowing = (course, social_id) => {
       this.followers[course] = this.followers[course] || {};
-      return this.followers[course].length !== 0 && _.includes(this.course(course).social_ids, social_id);
+      return this.courseFollowers(course).length !== 0 && _.includes(this.course(course).social_ids, social_id);
     }
 
-    this.course = (course) => _.head(this.followers[course])
+    this.courseFollowers = (course) => this.followers[course];
+
+    this.course = (course) => _.head(this.courseFollowers(course))
 
     this.addFollower = (course, social_id) => {
       this.course(course).social_ids = [...this.course(course).social_ids, social_id];
