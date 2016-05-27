e1259a6ff690a96a29cd51f2242fbd08998a413c
Federico Aloi <federico.aloi@gmail.com>
2016-05-26 22:06:55 -0300

    Moved ugly html regexp to a service

--- src/scripts/controllers/exercise-progress-controller.js
+++ src/scripts/controllers/exercise-progress-controller.js
@@ -1,7 +1,7 @@
 
 angular
   .module('classroom')
-  .controller('ExerciseProgressController', function ($scope, $state, $sce, $stateParams, $filter, toastr, exercisesProgress, Auth, Api, Breadcrumb) {
+  .controller('ExerciseProgressController', function ($scope, $state, $sce, $stateParams, $filter, toastr, exercisesProgress, containsHtml, Auth, Api, Breadcrumb) {
 
     const exerciseToView = _.find(exercisesProgress, (progress) => progress.exercise.id === Number($stateParams.eid));
 
@@ -46,7 +46,7 @@ angular
     $scope.indexNumber = ($index) => _.padStart($scope.begin() + $index + 1, 2, '0');
 
     $scope.trust = (html) => $sce.trustAsHtml(html);
-    $scope.containsHtml = (string) => /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/i.test(string);
+    $scope.containsHtml = containsHtml;
     $scope.selectDiff = (diff) => $scope.selectedDiff = diff;
     $scope.isSelectedDiff = (diff) => _.isEqual($scope.selectedDiff, diff);
 
