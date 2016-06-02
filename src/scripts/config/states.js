d6ccc7acab7f29042da246c40bbe238eafdb07f7
Agustin Pina <aguspina87@gmail.com>
2016-06-02 17:57:22 -0300

    Remove debugger

--- src/scripts/config/states.js
+++ src/scripts/config/states.js
@@ -153,7 +153,6 @@ angular
             controller: 'TeachersController',
             resolve: {
               teachers: ($state, $stateParams, Api) => {
-                debugger;
                 return Api
                   .getTeachers($stateParams)
                   .catch(() => $state.go('classroom.courses.course.guides', $stateParams, { location: 'replace' }));
