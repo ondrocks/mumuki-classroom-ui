8fda1d95b1a66fe28ef2aa234d9d9053b4c9f8d1
Federico Scarpa <fedescarpa@gmail.com>
2016-06-06 16:47:34 -0300

    added some logic to remember settings

--- src/scripts/services/remember-setting-service.js
+++ src/scripts/services/remember-setting-service.js
@@ -2,16 +2,36 @@
 angular
   .module('classroom')
   .service('RememberSetting', function($state, $cookies) {
-    return (scope, key) => {
+
+    const preferences = {
+      showDetails: false,
+      onlyFollowers: false,
+    };
+
+    const toggleField = (field) => {
+      preferences[field] = !preferences[field];
+      $cookies.putObject(field, { value: preferences[field] });
+    }
+
+    const RememberSetting = (scope, key) => {
       const cookieName = $state.current.name + "-" + key;
-      
-      scope[key] = _.get($cookies.getObject(cookieName), 'value'); 
+
+      scope[key] = _.get($cookies.getObject(cookieName), 'value');
       scope.$watch(key, (newValue, oldValue) => {
-        if (_.isNil(newValue)) { 
+        if (_.isNil(newValue)) {
           $cookies.remove(cookieName)
         } else {
           $cookies.putObject(cookieName, {value: newValue})
         }
       });
     };
+
+    RememberSetting.showDetails = () => preferences.showDetails;
+    RememberSetting.onlyFollowers = () => preferences.onlyFollowers;
+
+    RememberSetting.toggleShowDetails = () => toggleField('showDetails');
+    RememberSetting.toggleOnlyFollowers = () => toggleField('onlyFollowers');
+
+    return RememberSetting;
+
   });
