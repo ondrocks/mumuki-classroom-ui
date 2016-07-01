3767dacd8e241ee231ec46e539b76341c70a7194
Agustin Pina <aguspina87@gmail.com>
2016-07-01 12:49:47 -0300

    Remove directive to keep scope

--- src/scripts/controllers/guides-controller.js
+++ src/scripts/controllers/guides-controller.js
@@ -1,9 +1,14 @@
 
 angular
   .module('classroom')
-  .controller('GuidesController', function ($scope, $state, $stateParams, guides) {
+  .controller('GuidesController', function ($scope, $state, $stateParams, guides, Preferences) {
+    Preferences($scope, 'lastChapter');
+    if (_.isNil($scope.lastChapter)) $scope.lastChapter = { name: ''};
+    $scope.setLastChapterOpened = (chapter) => { $scope.lastChapter = { name: chapter }};
+
     const mapChapter = _.flow(_.lowerCase, _.deburr);
     $scope.setCount(guides.length);
+    $scope.actualChapter = (name) => name === $scope.lastChapter.name;
 
     $scope.dict = _.groupBy(guides, 'parent.type');
     $scope.chaptersAndLessons = _.groupBy($scope.dict.Lesson, (lesson) => mapChapter(lesson.parent.chapter.name));
