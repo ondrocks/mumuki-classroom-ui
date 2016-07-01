35e483aeff8ea34c4248a97503e58d75a751de59
Federico Scarpa <fedescarpa@gmail.com>
2016-07-01 17:55:31 -0300

    using browser locale

--- src/scripts/config/translate.js
+++ src/scripts/config/translate.js
@@ -3,10 +3,16 @@ angular
   .module('classroom')
   .config(function ($translateProvider, LANG_EN, LANG_ES) {
 
+    const supportedLocales = ['es', 'en'];
+
     $translateProvider.translations('es', LANG_ES);
     $translateProvider.translations('en', LANG_EN);
 
-    $translateProvider.preferredLanguage('es');
+    $translateProvider.determinePreferredLanguage(() => {
+      const locale = $translateProvider.resolveClientLocale().split('_')[0];
+      const isSupportedLocale = _.includes(supportedLocales, locale);
+      return isSupportedLocale ? locale : 'en';
+    });
 
     $translateProvider.useSanitizeValueStrategy(null);
 
