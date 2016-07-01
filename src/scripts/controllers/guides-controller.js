aea3648c3a9605b0d6d3a57d34c6a5bad925c860
Agustin Pina <aguspina87@gmail.com>
2016-07-01 00:19:49 -0300

    Change guides view to support topic info

--- src/scripts/controllers/guides-controller.js
+++ src/scripts/controllers/guides-controller.js
@@ -2,14 +2,17 @@
 angular
   .module('classroom')
   .controller('GuidesController', function ($scope, $state, $stateParams, guides) {
+    const mapChapter = _.flow(_.lowerCase, _.deburr);
     $scope.setCount(guides.length);
 
-    $scope.list = guides;
+    $scope.dict = _.groupBy(guides, 'parent.type');
+    $scope.chaptersAndLessons = _.groupBy($scope.dict.Lesson, (lesson) => mapChapter(lesson.parent.chapter.name));
+    $scope.others = $scope.dict.undefined;
     $scope.preTitle = _.capitalize($stateParams.course.toUpperCase());
     $scope.noItemsToShow = 'no_guides_to_show';
     $scope.inputPlaceholder = 'filter_available_guides';
 
-    $scope.sortCriteria = (guide) => ['language.name', 'name'];
+    $scope.sortCriteria = (guide) => ['parent.position', 'language.name', 'name'];
 
     $scope.open = (guide) => {
       const [ org, repo ] = guide.slug.split('/');
