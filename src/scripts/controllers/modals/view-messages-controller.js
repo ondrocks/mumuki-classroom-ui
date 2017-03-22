5cb56d60d0aabf77100310b2e48fcf3d97eadd9e
Federico Scarpa <fedescarpa@gmail.com>
2017-03-22 15:24:52 -0300

    added scroll to bottom on modal open

--- src/scripts/controllers/modals/view-messages-controller.js
+++ src/scripts/controllers/modals/view-messages-controller.js
@@ -1,11 +1,13 @@
 angular
   .module('classroom')
-  .controller('ViewMessagesController', function ($scope, $uibModalInstance, $sce, html, callback, Api) {
+  .controller('ViewMessagesController', function ($scope, $uibModalInstance, $sce, $timeout, html, callback, Api) {
 
     $scope.html = $sce.trustAsHtml(html);
 
     $scope.cancel = () => {
       $uibModalInstance.close();
     }
+
+    $timeout(() => $('.view-messages .chat').scrollTop($('.view-messages .chat')[0].scrollHeight));
 
   });
