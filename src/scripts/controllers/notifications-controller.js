c309b06662b95d519f6c40ca2a3ee918e635c037
Federico Scarpa <fedescarpa@gmail.com>
2017-05-09 10:47:49 -0300

    improve notifications paginator

--- src/scripts/controllers/notifications-controller.js
+++ src/scripts/controllers/notifications-controller.js
@@ -7,27 +7,30 @@ angular
 
     $scope.notifications = notifications;
 
-    const pageOffset = 3;
+    const pageOffset = 2;
+
+    $scope.limit = () => pageOffset * 2 + 1;
 
     $scope.totalPages = () => _.ceil($scope.notifications.total / Notification.perPage);
 
     $scope.actualPage = () => $scope.notifications.page;
-    $scope.offset = () => {
+    $scope.begin = () => {
       const total = $scope.totalPages();
       const actual = $scope.actualPage();
-      if (pageOffset < actual && actual < total + 1 - pageOffset) {
-        return actual - pageOffset;
-      } else if (actual <= pageOffset) {
+      if (actual <= pageOffset + 1) {
         return 0;
+      } else if (total - actual <= pageOffset) {
+        return total - $scope.limit();
       } else {
-        return total - pageOffset * 2 + 1;
+        return actual - pageOffset - 1;
       }
     }
 
     $scope.pad = (page) => _.padStart(page.toString(), 2, '0');
+
     $scope.pages = () => _.range(1, $scope.totalPages() + 1);
 
-    $scope.prevPage = () => Math.min(Math.max($scope.actualPage() - 1, 1), $scope.totalPages());
+    $scope.prevPage = () => Math.max($scope.actualPage() - 1, 1);
     $scope.nextPage = () => Math.min($scope.actualPage() + 1, $scope.totalPages());
 
   });
