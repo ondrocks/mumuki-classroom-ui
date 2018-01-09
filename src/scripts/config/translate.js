39770f7b1f204a41c4d1079b3b509addb8ba5acf
Julian Berbel Alt <julianberbel@hotmail.com>
2018-01-09 14:44:15 -0300

    Add portuguese to supported locales

--- src/scripts/config/translate.js
+++ src/scripts/config/translate.js
@@ -1,12 +1,13 @@
 
 angular
   .module('classroom')
-  .config(function ($translateProvider, LANG_EN, LANG_ES) {
+  .config(function ($translateProvider, LANG_EN, LANG_ES, LANG_PT) {
 
-    const supportedLocales = ['es', 'en'];
+    const supportedLocales = ['es', 'en', 'pt'];
 
     $translateProvider.translations('es', LANG_ES);
     $translateProvider.translations('en', LANG_EN);
+    $translateProvider.translations('pt', LANG_PT);
 
     $translateProvider.determinePreferredLanguage(() => {
       const locale = $translateProvider.resolveClientLocale().split('_')[0];
