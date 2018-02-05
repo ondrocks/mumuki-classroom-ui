ff24ba01f4aca215274570eddc2519805c19a9a5
Federico Scarpa <fedescarpa@gmail.com>
2018-02-05 16:28:52 -0300

    moving CSVs to assets folder

--- src/scripts/controllers/student-controller.js
+++ src/scripts/controllers/student-controller.js
@@ -14,26 +14,10 @@ angular
 
     const $translate = $filter('translate');
     const EMAIL_REGEX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
-    const exampleStudents = [
-      { first_name: 'example_student_1_first_name', last_name: 'example_student_1_last_name', email: 'example_student_1_email', personal_id: '12345678' },
-      { first_name: 'example_student_2_first_name', last_name: 'example_student_2_last_name', email: 'example_student_2_email', personal_id: '23456789' },
-    ]
 
     Breadcrumb.setCourse($stateParams.course);
 
-    const getCSVFile = () => {
-      let csvData = exampleStudents.map((s) => `${$translate(s.first_name)},${$translate(s.last_name)},${$translate(s.email)},${s.personal_id}`);
-      return 'first_name,last_name,email,personal_id\n' + csvData.join('\n');
-    }
-
     $scope.student = {};
-
-    $scope.getExampleCSVFile = () => {
-      var link = document.createElement('a');
-      link.download = `${$translate('example').toLowerCase()}.csv`;
-      link.href = `data:text/csv;charset=utf-8;base64,${btoa(getCSVFile())}`;
-      link.click();
-    }
 
     $scope.isTextValid = (text) => {
       return !_.isEmpty((text || '').trim())
