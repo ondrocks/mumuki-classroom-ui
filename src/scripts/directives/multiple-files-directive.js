ae8f3622ee66997a08eca383d8fafd7d4d978b4a
Rodrigo Alfonso <rodri042@gmail.com>
2018-12-06 04:05:50 -0300

    Adding highlighting suport

--- src/scripts/directives/multiple-files-directive.js
+++ src/scripts/directives/multiple-files-directive.js
@@ -1,38 +1,38 @@
 angular
   .module('classroom')
-  .directive('multipleFiles', function (Languages) {
+  .directive('multipleFiles', function (Languages, Api) {
+    const makePromiseFromObject = (obj) => {
+      const keys = Object.keys(obj);
+      const values = Object.values(obj);
+      return Promise.all(values).then(resolved => {
+        const res = {};
+        for (let i = 0; i < keys.length; i += 1) res[keys[i]] = resolved[i];
+        return res;
+      });
+    };
 
     return {
-
       restrict: 'E',
       templateUrl: 'views/directives/multiple-files.html',
       scope: {
         tabs: '&'
       },
       controller: ($scope) => {
-        $scope.data = $scope.tabs();
-
         let activeTab;
-
         const selectFirstTab = () => activeTab = _.chain($scope.data).keys().first().value();
         const extension = (key) => _.chain(key).split('.').last().value();
+        $scope.select = (key) => { activeTab = key }
+        $scope.isActive = (key) => activeTab === key;
 
-        $scope.select = (key) => {
-          activeTab = key
-        }
+        const $files = _.mapValues($scope.tabs(), (v, k) => {
+          return Api.renderCode(extension(k), v);
+        });
 
-        $scope.isActive = (key) => {
-          return activeTab === key;
-        }
-
-        $scope.getAceModeFromExtension = (key) => {
-          return "javascript"; // TODO: Fix
-          return Languages.getAceModeFromExtension(extension(key));
-        }
-
-        selectFirstTab();
+        makePromiseFromObject($files).then((files) => {
+          $scope.data = files;
+          selectFirstTab();
+        });
       }
-
     }
 
   })
